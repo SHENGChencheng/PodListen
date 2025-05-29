566122b30f381ea59bcdc27ac62410e8b650d640
shengchencheng <shengchencheng@foxmail.com>
2025-05-29 10:10:34 +0800

    feat: calculateDisplayFeatures

--- app/src/main/java/com/podlisten/android/MainActivity.kt
+++ app/src/main/java/com/podlisten/android/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.adaptive.calculateDisplayFeatures
 import com.podlisten.android.ui.theme.PodListenTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val displayFeatures = calculateDisplayFeatures(this)
+
             PodListenTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
