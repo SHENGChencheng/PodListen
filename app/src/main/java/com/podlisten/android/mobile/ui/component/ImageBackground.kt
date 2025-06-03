209ba04b5cd17e25e144d3a64403fb663da5e14a
shengchencheng <shengchencheng@foxmail.com>
2025-06-03 15:36:36 +0800

    feat: PlayerContentWithBackground

---
+++ app/src/main/java/com/podlisten/android/mobile/ui/component/ImageBackground.kt
@@ -0,0 +1,69 @@
+package com.podlisten.android.mobile.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
+
+@Composable
+fun ImageBackgroundColorScrim(
+    url: String?,
+    color: Color,
+    modifier: Modifier = Modifier,
+) {
+    ImageBackground(
+        url = url,
+        modifier = modifier,
+        overlay = {
+            drawRect(color)
+        }
+    )
+}
+
+@Composable
+fun ImageBackgroundRadialGradientScrim(
+    url: String?,
+    colors: List<Color>,
+    modifier: Modifier = Modifier,
+) {
+    ImageBackground(
+        url = url,
+        modifier = modifier,
+        overlay = {
+            val brush = Brush.radialGradient(
+                colors = colors,
+                center = Offset(0f, size.height),
+                radius = size.width * 1.5f,
+            )
+            drawRect(brush, blendMode = BlendMode.Multiply)
+        }
+    )
+}
+
+@Composable
+fun ImageBackground(
+    modifier: Modifier = Modifier,
+    url: String?,
+    overlay: DrawScope.() -> Unit,
+) {
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .fillMaxWidth()
+            .drawWithCache {
+                onDrawWithContent {
+                    drawContent()
+                    overlay()
+                }
+            }
+    )
+}
--- app/src/main/java/com/podlisten/android/mobile/ui/player/PlayerScreen.kt
+++ app/src/main/java/com/podlisten/android/mobile/ui/player/PlayerScreen.kt
@@ -1,9 +1,31 @@
 package com.podlisten.android.mobile.ui.player
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.window.core.layout.WindowSizeClass
 import androidx.window.layout.DisplayFeature
+import com.podlisten.android.R
+import com.podlisten.android.core.domain.player.model.PlayerEpisode
+import com.podlisten.android.mobile.ui.component.ImageBackgroundColorScrim
+import kotlinx.coroutines.launch
+import java.time.Duration
 
 @Composable
 fun PlayerScreen(
@@ -12,5 +34,131 @@ fun PlayerScreen(
     onBackPress: () -> Unit,
     viewModel: PlayerViewModel = hiltViewModel(),
 ) {
+    val uiState = viewModel.uiState
+    PlayerScreen(
+        uiState = uiState,
+        windowSizeClass = windowSizeClass,
+        displayFeatures = displayFeatures,
+        onBackPress = onBackPress,
+        onAddToQueue = viewModel::onAddToQueue,
+        onStop = viewModel::onStop,
+        playerControlActions = PlayerControlActions(
+            onPlayPress = viewModel::onPlay,
+            onPausePress = viewModel::onPause,
+            onAdvanceBy = viewModel::onAdvancedBy,
+            onRewindBy = viewModel::onRewindBy,
+            onNext = viewModel::onNext,
+            onPrevious = viewModel::onPrevious,
+            onSeekingStarted = viewModel::onSeekingStarted,
+            onSeekingFinished = viewModel::onSeekingFinished,
+        )
+    )
+}
+
+@Composable
+private fun PlayerScreen(
+    modifier: Modifier = Modifier,
+    uiState: PlayerUiState,
+    windowSizeClass: WindowSizeClass,
+    displayFeatures: List<DisplayFeature>,
+    onBackPress: () -> Unit,
+    onAddToQueue: () -> Unit,
+    onStop: () -> Unit,
+    playerControlActions: PlayerControlActions,
+) {
+    DisposableEffect(Unit) {
+        onDispose {
+            onStop()
+        }
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+    val snackBatText = stringResource(R.string.episode_added_to_your_queue)
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { contentPadding ->
+        if (uiState.episodePlayerState.currentEpisode != null) {
+            PlayerContentWithBackground(
+                uiState = uiState,
+                windowSizeClass = windowSizeClass,
+                displayFeatures = displayFeatures,
+                onBackPress = onBackPress,
+                onAddToQueue = {
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(snackBatText)
+                    }
+                    onAddToQueue()
+                },
+                playerControlActions = playerControlActions,
+                contentPadding = contentPadding
+            )
+        } else {
+            FullScreenLoading()
+        }
+    }
+}
+
+@Composable
+private fun PlayerBackground(
+    modifier: Modifier,
+    episode: PlayerEpisode?,
+) {
+    ImageBackgroundColorScrim(
+        url = episode?.podcastImageUrl,
+        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.9f),
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun PlayerContentWithBackground(
+    modifier: Modifier = Modifier,
+    uiState: PlayerUiState,
+    windowSizeClass: WindowSizeClass,
+    displayFeatures: List<DisplayFeature>,
+    onBackPress: () -> Unit,
+    onAddToQueue: () -> Unit,
+    playerControlActions: PlayerControlActions,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        PlayerBackground(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding),
+            episode = uiState.episodePlayerState.currentEpisode
+        )
+        PlayerContent()
+    }
+}
+
+@Composable
+fun PlayerContent() {
 
 }
+
+@Composable
+private fun FullScreenLoading(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+data class PlayerControlActions(
+    val onPlayPress: () -> Unit,
+    val onPausePress: () -> Unit,
+    val onAdvanceBy: (Duration) -> Unit,
+    val onRewindBy: (Duration) -> Unit,
+    val onNext: () -> Unit,
+    val onPrevious: () -> Unit,
+    val onSeekingStarted: () -> Unit,
+    val onSeekingFinished: (newElapsed: Duration) -> Unit,
+)
